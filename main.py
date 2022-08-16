d7a42398d0f7bbe02677bbe64507e28a0b37852f
Vladimir Malkov <tmnsit@gmail.com>
2022-08-16 14:26:37 +0500

    finish

--- main.py
+++ main.py
@@ -1,159 +1,145 @@
 import requests
 from dotenv import dotenv_values
 import json
+import math
 
 from helper import Helper
-from models.bank import Bank, Program, City
 
 config = dotenv_values(".env")
 
-url_old_bank = config['HOST_OLD_BANKS'] + '/api/mortgage'
-url_old_filter = config['HOST_OLD_BANKS'] + '/api/mortgage/filter'
-url_new_bank = config['HOST_NEW_BANKS'] + '/products/hypothec/api/group/'
-
-cities = {}
-programs = {}
-banks = {}
-mortgages = {}
 
 
-def get_item_banks():
-    r = requests.get(url_old_bank)
-    banks_json = r.json()['data']
-    for bank in banks_json:
-        _programs = list[Program]
-        _arr_programs = bank['programs']
-        _city_name = bank['city']
-        del bank['city']
-        bank['programs'] = []
-        _bank_row = json.dumps(bank, ensure_ascii=False, indent=4)
-        _bank_entity = Bank.parse_raw(_bank_row)
-        for program_key in _arr_programs:
-            if _bank_entity.program is None:
-                _bank_entity.program = []
-            _bank_entity.program.append(programs[program_key])
-        _bank_entity.city = cities[_city_name]
-        mortgages[_bank_entity.id] = _bank_entity
+class Parser:
+    cities_collection: list
+    programs_collection: list
+    banks_collection: list
 
+    def get_colletions(self):
+        url_collections = config['HOST_SERVER'] + config['PATH_GET_BANK_COLLECTION']
+        r = requests.get(url_collections)
+        collections_data = r.json()['data']
+        self.cities_collection = collections_data['cities']
+        self.programs_collection = collections_data['programs']
+        self.banks_collection = collections_data['banks']
 
-def parse_old():
-    r = requests.get(url_old_filter)
-    filter_json = r.json()['data']
-    for item in filter_json:
-        if item['name'] == 'program':
-            for option in item['options']:
-                row = json.dumps(option, ensure_ascii=False, indent=4)
-                programs[option['value']] = Program.parse_raw(row)
-        if item['name'] == 'city_id':
-            for option in item['options']:
-                option['code'] = Helper.transform_city_name(option['text'])
-                row = json.dumps(option, ensure_ascii=False, indent=4)
-                cities[option['text']] = City.parse_raw(row)
-    r.close()
+    def start_parse(self):
+        print('Start parsing.....')
+        self.get_colletions()
+        self.get_new_banks_to_json()
+        _banks = self.tranform_data_for_server()
+        self.save_finish_file_json({"mortgages": _banks})
+        self.send_json_to_server(data=_banks)
 
+    def send_json_to_server(self, data):
+        _request_json = json.dumps({"banks": data}, ensure_ascii=False).encode('utf-8')
+        url = config['HOST_SERVER'] + config['PATH_SAVE_API']
+        r = requests.post(url, data=_request_json,
+                          headers={"Content-Type": "application/json; charset=utf-8",
+                                   "Accept": "application/json; charset=utf-8"})
+        print(r.json())
 
-def get_new_banks_to_json():
-    payload = {
-        "source": "submenu_hypothec",
-        "sort": "popular",
-        "order": "desc",
-        "isRefinance": 0
-    }
-    headers = {
-        "x-requested-with": "XMLHttpRequest",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-    }
-
-    count_page = 1
-    city_names = []
-    for city_key in cities:
-        _city: City = cities[city_key]
+    def get_new_banks_to_json(self):
+        url_new_bank = config['HOST_NEW_BANKS'] + '/products/hypothec/api/group/'
+        payload = {
+            "source": "submenu_hypothec",
+            "sort": "popular",
+            "order": "desc",
+            "isRefinance": 0
+        }
+        headers = {
+            "x-requested-with": "XMLHttpRequest",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+        }
+        city_names = []
         new_banks = {}
-        while True:
-            print(f"stage:{count_page}")
-            url = f"{url_new_bank}{_city.code}/"
-            payload['page'] = count_page
-            r = requests.get(url, headers=headers, params=payload)
-            bank_json = r.json()
-            r.close()
-            if len(bank_json['grouped_table']) == 0:
-                break
-            for row_bank in bank_json['grouped_table']:
-                for ipoteka in row_bank['credit_result_rows']:
-                    if not city_key in new_banks:
-                        new_banks[city_key] = {}
-                    if not ipoteka['bank_name'].lower() in new_banks[city_key]:
-                        new_banks[city_key][ipoteka['bank_name'].lower()] = {}
-                    new_banks[city_key][ipoteka['bank_name'].lower()][ipoteka['mortgage_name'].lower()] = ipoteka
-            count_page = count_page + 1
+
+        for _city in self.cities_collection:
+            count_page = 1
+            _city_code = Helper.transform_city_name(_city["name"].lower())
+            while True:
+                print(f"stage:{count_page}")
+                url = f"{url_new_bank}{_city_code}/"
+                payload['page'] = count_page
+                r = requests.get(url, headers=headers, params=payload)
+                bank_json = r.json()
+                r.close()
+
+                if len(bank_json['grouped_table']) == 0:
+                    break
+                for row_bank in bank_json['grouped_table']:
+                    for ipoteka in row_bank['credit_result_rows']:
+                        _city_name = _city["name"].lower()
+                        if not _city_name in new_banks:
+                            new_banks[_city_name] = {}
+                        if not ipoteka['bank_name'].lower() in new_banks[_city_name]:
+                            new_banks[_city_name][ipoteka['bank_name'].lower()] = {}
+                        new_banks[_city_name][ipoteka['bank_name'].lower()][
+                            ipoteka['mortgage_name'].lower()] = ipoteka
+                count_page = count_page + 1
         with open('upload/data.json', 'w', encoding='utf-8') as f:
             json.dump(new_banks, f, ensure_ascii=False, indent=4)
-        city_names.append(city_key)
 
+    def save_finish_file_json(self, data):
+        with open('upload/finish.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
-def get_new_banks_from_json():
-    with open('upload/data.json', 'r', encoding='utf-8') as f:
-        _ipoteka = json.load(f)
-        return _ipoteka
+    def get_new_banks_from_json(self):
+        with open('upload/data.json', 'r', encoding='utf-8') as f:
+            _ipoteka = json.load(f)
+            return _ipoteka
 
+    def tranform_data_for_server(self):
+        _mortgages_file = self.get_new_banks_from_json()
+        banks = []
+        for city in self.cities_collection:
+            _city_name = city['name'].lower()
+            for _city_key in _mortgages_file.keys():
+                if _city_name in _city_key:
+                    for bank in self.banks_collection:
+                        _bank_name = bank['name'].lower()
+                        for _bank_key in _mortgages_file[_city_key].keys():
+                            if _bank_name in _bank_key:
+                                for program in self.programs_collection:
+                                    _program_name = program['name'].lower()
+                                    for _program_key in _mortgages_file[_city_key][_bank_key].keys():
+                                        if _program_name in _program_key:
+                                            _new_bank = self.arr_prepair(
+                                                _mortgages_file[_city_key][_bank_key][_program_key], bank_id=bank['id'],
+                                                city_id=city['id'], program_id=program['id'])
+                                            banks.append(_new_bank)
+        return banks
 
-def tranform_data_for_server():
-    _mortgages_file = get_new_banks_from_json()
-    for _mortgage_key in mortgages:
-        _bank_name = mortgages[_mortgage_key].name
-        _city_name = mortgages[_mortgage_key].city.text
-        _program_name = mortgages[_mortgage_key].program[0].text
-        if _city_name in _mortgages_file:
-            if _bank_name.lower() in _mortgages_file[_city_name]:
-                if _program_name.lower() in _mortgages_file[_city_name][_bank_name.lower()]:
-                    _program = _mortgages_file[_city_name][_bank_name.lower()][_program_name.lower()]
-                    mortgages[_mortgage_key].rate = _program['rate_min']
-                    mortgages[_mortgage_key].amount_of_credit.min = _program['amount_from']
-                    mortgages[_mortgage_key].amount_of_credit.max = _program['amount_to']
-                    mortgages[_mortgage_key].firstPayment.min = _program['initial_fee_from']
-                    mortgages[_mortgage_key].firstPayment.max = _program['initial_fee_from']
-                    mortgages[_mortgage_key].term.min = round(_program['period_from'] / 365)
-                    mortgages[_mortgage_key].term.max = round(_program['period_to'] / 365)
-                    mortgages[_mortgage_key].apart_price.min = _program['amount_from'] - round(_program['amount_from'] / 100 * _program['initial_fee_from'])
-                    mortgages[_mortgage_key].apart_price.max = _program['amount_to'] - round(_program['amount_to'] / 100 * _program['initial_fee_from'])
-    list_mortgage = []
-    for mor_key in mortgages:
-        _mortgage: Bank = mortgages[mor_key]
-        mortgage_out = {
-            "id": _mortgage.id,
-            "name": _mortgage.name,
-            "city": _mortgage.city.id,
-            "rate": _mortgage.rate,
-            "img": _mortgage.img,
+    def arr_prepair(self, arr_bank, bank_id, city_id, program_id):
+        _bank = {
+            "id": bank_id,
+            "city": city_id,
+            "rate": arr_bank['rate_min'],
             "term": {
-                "min": _mortgage.term.min,
-                "max": _mortgage.term.max
+                "min": round(arr_bank['period_from'] / 365),
+                "max": round(arr_bank['period_to'] / 365)
             },
             "apart-price": {
-                "min": _mortgage.apart_price.min,
-                "max": _mortgage.apart_price.max
+                "min": round(arr_bank['amount_from'] / (1 - (arr_bank['initial_fee_from'] / 100))),
+                "max": round(arr_bank['amount_to'] / (1 - (95 / 100)))
             },
             "amount-of-credit": {
-                "min": _mortgage.amount_of_credit.min,
-                "max": _mortgage.amount_of_credit.max
+                "min": arr_bank['amount_from'],
+                "max": arr_bank['amount_to']
             },
             "first-payment": {
-                "min": _mortgage.firstPayment.min,
-                "max": _mortgage.firstPayment.max
+                "min": arr_bank['initial_fee_from'],
+                "max": 95
             },
-            "programs": [_mortgage.program[0].id]
+            "programs": [
+                program_id
+            ]
         }
-        list_mortgage.append(mortgage_out)
-    with open('upload/finish.json', 'w', encoding='utf-8') as f:
-        json.dump({"data": list_mortgage}, f, ensure_ascii=False, indent=4)
+        return _bank
 
 
 def main():
-    print('start parser...')
-    parse_old()
-    get_item_banks()
-    get_new_banks_to_json()
-    tranform_data_for_server()
+    parser = Parser()
+    parser.start_parse()
 
 
 if __name__ == '__main__':
